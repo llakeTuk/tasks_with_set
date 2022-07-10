474f314cac73b510d93a523b1a0f2fc1fb16a5ba
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-07-10 15:37:46 +0300

    Add files via upload

---
+++ Set_2.py
@@ -0,0 +1,14 @@
+from collections.abc import Hashable
+
+def list_to_set(input_list):
+  for element in input_list:
+    if isinstance(element, Hashable):
+      
+  st = set(input_string)
+  return 
+y = 'y'
+while y == 'y':
+  input_data = input('enter string: ')
+  print(to_set(input_data))
+  print(*to_set(input_data))
+  y = input('restart?(y/n): ')
