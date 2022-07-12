66d8bf3e442a5b5a3472f48dd2c6e6c81419c195
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-07-12 18:15:48 +0300

    Add files via upload

---
+++ Set_3.2.py
@@ -0,0 +1,19 @@
+
+def difference(set_1, set_2, set_3, symmetric = True):
+  if symmetric:
+    return set_1.symmetric_difference(set_2).symmetric_difference(set_3)
+  return set_1.differemce(set_2, set_3)
+y = 'y'
+while y == 'y':
+  print('enter 3 sets of elements, using comma')
+  set_1 = set(input('enter 1 set: ').split(','))
+  set_2 = set(input('enter 2 set: ').split(','))
+  set_3 = set(input('enter 3 set: ').split(','))
+  symm = input('symmetric difference?(y/n): ')
+  if symm == 'y':
+    print(difference(set_1, set_2, set_3, symmetric = True))
+  elif symm == 'n':
+    print(difference(set_1, set_2, set_3, symmetric = False))
+  else:
+    print('invalid symbol')
+  y = input('restart?(y/n): ')
